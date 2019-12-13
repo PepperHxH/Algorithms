4d0c43770f8545c512b1a3be6b4c25774fa25380
PepperHxH <838245976@qq.com>
2019-12-13 22:58:45 +0800

    2019/12/13

--- Algorithms/130SurroundedRegions.java
+++ Algorithms/130SurroundedRegions.java
@@ -1,3 +1,4 @@
+//递归的dfs方法
 class Solution {
     int[][] direction = {{0,1}, {0,-1}, {-1,0}, {1,0}};
     int m, n;
@@ -34,6 +35,52 @@ class Solution {
         }
         return ;
     }
-
 }
+
+/*
+//用队列实现的bfs方法 效率低一些
+class Solution {
+    int[][] direction = {{0,1}, {0,-1}, {-1,0}, {1,0}};
+    int m, n;
+
+    public void solve(char[][] board) {
+        if(board == null || board.length == 0) return;
+        m = board.length;
+        n = board[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if((i == 0 || i == m-1 || j == 0 || j == n-1) && board[i][j] == 'O') {
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.offer(new int[]{i, j});
+                    while(queue.size() != 0) {
+                        int size = queue.size();
+                        board[i][j] = 'T';
+                        while(size-- > 0) {
+                            int[] tmp = queue.poll();
+                            for(int[] d : direction) {
+                                int x = tmp[0] + d[0];
+                                int y = tmp[1] + d[1];
+                                if(x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') continue;
+                                queue.offer(new int[]{x, y});
+                                board[x][y] = 'T';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+            
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                } else if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        return;
+    }
+}
+*/
 
